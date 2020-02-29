0c17ce72d1cc803188fc6ae9050af7b36abbb226
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-29 09:11:25 -0300

    Creando el Bloque

---
+++ lapizzeria-gutenberg.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+    Plugin Name: La Pizzeria Gutenberg Blocks
+    Plugin URI:
+    Description: Agrega bloques de Gutenberg nativos
+    Version: 1.0
+    Author: Jorge Barcos
+    Author URI: https://jorgebarcos.com
+    License: GPL2
+    License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if(!defined('ABSPATH')) exit;
