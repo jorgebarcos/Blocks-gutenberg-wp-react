1db550a346eb9d33bf6dfe74413b6aa2c8c8c526
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-29 09:22:58 -0300

    Registrando el Archivo principal para los bloques

--- lapizzeria-gutenberg.php
+++ lapizzeria-gutenberg.php
@@ -12,3 +12,22 @@
 */
 
 if(!defined('ABSPATH')) exit;
+
+/* Registrar bloques, scripts y css */
+
+function lapizzeria_registrar_bloques() {
+
+    // Si gutenber no existe, salir
+    if(!function_exists('register_block_type')) {
+        return;
+    }
+
+    // Registrar los bloques en el editor
+    wp_register_script(
+        'lapizzeria-editor-script',  // nombre unico
+        plugins_url( 'build/index.js', __FILE__), // archivo con los bloques
+        array('wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor'), // dependencias
+        filemtime( plugin_dir_path(__FILE__) . 'build/index.js') // Versión
+    );
+}
+add_action('init', 'lapizzeria_registrar_bloques');
